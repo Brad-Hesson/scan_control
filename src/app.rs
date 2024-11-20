daec46e19814104945b5b07230615ee88afb4970
Brad Hesson <brad.hesson@outlook.com>
2024-11-20 03:07:34 -0700

    added screen transformation

--- src/app.rs
+++ src/app.rs
@@ -3,14 +3,13 @@ use std::{borrow::Cow, sync::Arc};
 use bytemuck::bytes_of;
 use eframe::{
     egui_wgpu::{self, CallbackTrait, RenderState},
-    wgpu::{self, util::DeviceExt, BindGroup, Buffer, Device, Queue, RenderPipeline},
+    wgpu::{self, util::DeviceExt, BindGroup, Buffer, Queue, RenderPipeline},
 };
-use egui::Vec2;
-use glam::Mat4;
+use glam::{Affine2, Mat3, Mat4, Vec2, Vec4};
 
 pub struct MyApp {
     /// Behind an `Arc<Mutex<…>>` so we can pass it to [`egui::PaintCallback`] and paint later.
-    world_transform: glam::Mat4,
+    world_transform: Affine2,
     triangle: ImageCallback,
 }
 
@@ -18,7 +17,7 @@ impl MyApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         let wgpu = cc.wgpu_render_state.as_ref().unwrap();
         Self {
-            world_transform: glam::Affine3A::IDENTITY.into(),
+            world_transform: Affine2::IDENTITY,
             triangle: ImageCallback::new(wgpu),
         }
     }
@@ -42,32 +41,52 @@ impl eframe::App for MyApp {
     }
 }
 
+macro_rules! v2 {
+    ($rest:expr) => {
+        Vec2::from(mint::Vector2::from($rest))
+    };
+}
+
 impl MyApp {
     fn custom_painting(&mut self, ui: &mut egui::Ui) {
-        let (rect, response) = ui.allocate_at_least(
-            ui.available_size_before_wrap() - Vec2::Y * 20.,
-            egui::Sense::drag(),
-        );
-        let del = Mat4::from_translation(
-            glam::Vec3::new(response.drag_motion().x, -response.drag_motion().y, 0.0) / 100.,
-        );
-        self.world_transform = del * self.world_transform;
+        let (rect, response) =
+            ui.allocate_at_least(ui.available_size_before_wrap(), egui::Sense::drag());
+
+        let drag = Affine2::from_translation(v2!(response.drag_delta()));
+        let zoom = if let Some(window_pos) = response.hover_pos() {
+            let scalar = (ui.input(|is| is.raw_scroll_delta).y / 100.).exp();
+            let scale = Affine2::from_scale(Vec2::splat(scalar));
+            let trans = Affine2::from_translation(v2!(window_pos - rect.center()));
+            trans * scale * trans.inverse()
+        } else {
+            Affine2::IDENTITY
+        };
+        self.world_transform = zoom * drag * self.world_transform;
+
+        let screen_transform =
+            Affine2::from_scale(v2!(rect.size()) * Vec2::new(0.5, -0.5)).inverse();
+
+        let mat4 = affine2_to_mat4(screen_transform * self.world_transform);
 
         // Clone locals so we can move them into the paint callback:
-        self.triangle.queue.write_buffer(
-            &self.triangle.uniform_buf,
-            0,
-            bytes_of(self.world_transform.as_ref()),
-        );
+        self.triangle
+            .queue
+            .write_buffer(&self.triangle.uniform_buf, 0, bytes_of(mat4.as_ref()));
         self.triangle.queue.submit([]);
         let callback = egui_wgpu::Callback::new_paint_callback(rect, self.triangle.clone());
         ui.painter().add(callback);
     }
 }
 
+fn affine2_to_mat4(af: Affine2) -> Mat4 {
+    let mut mat4 = Mat4::from_mat3(Mat3::from_mat2(af.matrix2));
+    let trans = af.translation;
+    mat4.w_axis = Vec4::new(trans.x, trans.y, 0., 1.);
+    mat4
+}
+
 #[derive(Clone)]
 struct ImageCallback {
-    device: Arc<Device>,
     pipeline: Arc<RenderPipeline>,
     queue: Arc<Queue>,
     bind_group: Arc<BindGroup>,
@@ -151,7 +170,6 @@ impl ImageCallback {
             label: None,
         });
         Self {
-            device: wgpu.device.clone(),
             pipeline: Arc::new(pipeline),
             queue: wgpu.queue.clone(),
             bind_group: Arc::new(bind_group),
@@ -171,4 +189,3 @@ impl CallbackTrait for ImageCallback {
         render_pass.draw(0..4, 0..1);
     }
 }
-
