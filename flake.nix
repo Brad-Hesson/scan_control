559687f919b0146dc35ee14206ac9007c96b52dd
Brad Hesson <brad.hesson@outlook.com>
2024-11-23 14:30:36 -0700

    added wgsl_analyzer to flake

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
   };
   outputs = flakes: flakes.flake-utils.lib.eachDefaultSystem (system:
     let
@@ -9,12 +10,25 @@
         inherit system;
         config.allowUnfree = true;
       };
+      craneLib = flakes.crane.mkLib pkgs;
+      wgsl-analyzer = craneLib.buildPackage {
+        pname = "wgsl_analyzer";
+        version = "0.0.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "wgsl-analyzer";
+          repo = "wgsl-analyzer";
+          rev = "v0.8.1";
+          hash = "sha256-bhosTihbW89vkqp1ua0C1HGLJJdCNfRde98z4+IjkOc=";
+        };
+        doCheck = false;
+      };
     in
     {
       devShell = pkgs.mkShell rec {
         packages = [
           pkgs.rustup
           pkgs.renderdoc
+          wgsl-analyzer
         ];
         buildInputs = with pkgs; [
           # necessary for building wgpu in 3rd party packages (in most cases)
@@ -42,6 +56,9 @@
           vulkan-validation-layers # don't need them *strictly* but immensely helpful
         ];
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        shellHook = ''
+          sed -i '/wgsl-analyzer\.server\.path/c\    \"wgsl-analyzer\.server\.path\": \"${wgsl-analyzer}/bin/wgsl_analyzer",' .vscode/settings.json
+        '';
       };
     }
   );
