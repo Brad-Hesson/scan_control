91e5459314b299fb81af1c06a871228298c9a435
Brad Hesson <brad.hesson@outlook.com>
2026-03-17 16:31:08 -0600

    fixed many bugs in nanonis connection

--- src/connection/nanonis_connection.rs
+++ src/connection/nanonis_connection.rs
@@ -1,5 +1,4 @@
 use core::f32;
-use std::sync::mpsc::Sender;
 
 use glam::Affine2;
 use itertools::{izip, Itertools};
@@ -15,8 +14,8 @@ use crate::scan_view::{ImageEncoder, StaticImage};
 pub struct NanonisConnection {
     conn: blocking::NanonisTcp,
     pub live_image: StaticImage,
-    stamp_rx: std::sync::mpsc::Receiver<StaticImage>,
-    frame_rx: std::sync::mpsc::Receiver<FrameDataGrabResponse>,
+    stamp_rx: watch::Receiver<Option<FrameDataGrabResponse>>,
+    frame_rx: watch::Receiver<Option<FrameDataGrabResponse>>,
     scanning_rx: watch::Receiver<bool>,
     transform_rx: watch::Receiver<Affine2>,
     size_rx: watch::Receiver<[u32; 2]>,
@@ -24,6 +23,7 @@ pub struct NanonisConnection {
     signal_names_rx: watch::Receiver<Vec<String>>,
     channels_rx: watch::Receiver<Vec<usize>>,
     channel_tx: watch::Sender<Option<usize>>,
+    num_stamps: usize,
 }
 
 impl NanonisConnection {
@@ -42,25 +42,28 @@ impl NanonisConnection {
             .into_iter()
             .map(|v| v as usize)
             .collect_vec();
+        let channel = channels
+            .contains(&30)
+            .then_some(30)
+            .or(channels.get(0).copied());
 
-        let (stamp_tx, stamp_rx) = std::sync::mpsc::channel();
-        let (frame_tx, frame_rx) = std::sync::mpsc::channel();
+        let (stamp_tx, stamp_rx) = watch::channel(None);
+        let (frame_tx, frame_rx) = watch::channel(None);
         let (scanning_tx, scanning_rx) = watch::channel(true);
         let (transform_tx, transform_rx) = watch::channel(transform);
         let (size_tx, size_rx) = watch::channel(size);
         let (name_tx, name_rx) = watch::channel(name.clone());
         let (signal_names_tx, signal_names_rx) = watch::channel(sig_names.clone());
         let (channels_tx, channels_rx) = watch::channel(channels.clone());
-        let (channel_tx, channel_rx) = watch::channel(None);
+        let (channel_tx, channel_rx) = watch::channel(channel);
 
         tokio::spawn(scan_worker(
             (address.as_ref().to_string(), 6502),
             ctx.clone(),
-            image_encoder.clone(),
             stamp_tx,
             scanning_tx.clone(),
             channel_rx.clone(),
-            signal_names_rx.clone(),
+            frame_rx.clone(),
         ));
         tokio::spawn(line_worker(
             (address.as_ref().to_string(), 6503),
@@ -84,6 +87,7 @@ impl NanonisConnection {
         live_image.channels = channels;
         live_image.signal_names = sig_names;
         live_image.name = name;
+        live_image.channel = channel;
 
         Self {
             size_rx,
@@ -97,9 +101,10 @@ impl NanonisConnection {
             signal_names_rx,
             channels_rx,
             channel_tx,
+            num_stamps: 0,
         }
     }
-    pub fn update(&mut self, encoder: &ImageEncoder) -> Vec<StaticImage> {
+    pub fn update(&mut self, encoder: &ImageEncoder) -> Option<StaticImage> {
         // update watchers from local vars
         self.channel_tx.send_if_modified(|prev| {
             if *prev != self.live_image.channel {
@@ -132,82 +137,93 @@ impl NanonisConnection {
             self.live_image.transform = *self.transform_rx.borrow_and_update();
         }
         // Check for new live frames
-        let mut new_frame = None;
-        while let Ok(frame) = self.frame_rx.try_recv() {
-            new_frame = Some(frame);
-        }
-        if let Some(frame) = new_frame {
-            self.live_image
-                .write_lines(encoder, .., |buf| {
-                    buf.copy_from_slice(&frame.scan_data.data)
-                })
-                .unwrap();
-            self.live_image.update_texture(encoder);
+        if self.frame_rx.has_changed().unwrap() {
+            if let Some(frame) = self.frame_rx.borrow_and_update().as_ref() {
+                self.live_image
+                    .write_lines(encoder, .., |buf| {
+                        buf.copy_from_slice(&frame.scan_data.data)
+                    })
+                    .unwrap();
+                self.live_image.update_texture(encoder);
+            }
         }
 
         // Check for full frame stamps
-        let mut stamps = Vec::new();
-        while let Ok(mut image) = self.stamp_rx.try_recv() {
-            image.norm_type = self.live_image.norm_type;
-            image.std_dev = self.live_image.std_dev;
-            image.fit_type = self.live_image.fit_type;
-            image.update_texture(encoder);
-            stamps.push(image);
+        let mut stamp = None;
+        if self.stamp_rx.has_changed().unwrap() {
+            if let Some(frame) = self.stamp_rx.borrow_and_update().as_ref() {
+                self.live_image.clear_texture(encoder);
+                let mut image = StaticImage::new(
+                    encoder,
+                    self.live_image.size(),
+                    self.live_image.transform,
+                    |buf| buf.copy_from_slice(&frame.scan_data.data),
+                );
+                image.name = format!("{}({})", self.live_image.name, self.num_stamps);
+                self.num_stamps += 1;
+                image.norm_type = self.live_image.norm_type;
+                image.std_dev = self.live_image.std_dev;
+                image.fit_type = self.live_image.fit_type;
+                image.update_texture(encoder);
+                image.channel = self.live_image.channel;
+                image.signal_names = self.live_image.signal_names.clone();
+                image.channels = vec![self.live_image.channel.unwrap()];
+                stamp = Some(image);
+            }
         }
-        if !stamps.is_empty() {
-            self.live_image.clear_texture(encoder);
-        }
-        stamps
+        stamp
     }
 }
 
 async fn scan_worker(
     addr: impl ToSocketAddrs,
     ctx: egui::Context,
-    encoder: ImageEncoder,
-    frame_tx: Sender<StaticImage>,
+    stamp_tx: watch::Sender<Option<FrameDataGrabResponse>>,
     scanning: watch::Sender<bool>,
     channel_rx: watch::Receiver<Option<usize>>,
-    signal_names_rx: watch::Receiver<Vec<String>>,
+    frame_rx: watch::Receiver<Option<FrameDataGrabResponse>>,
 ) {
     let mut conn = nonblocking::NanonisTcp::new(addr).await.unwrap();
-    let mut stamp_num = 0;
     loop {
         conn.scan_wait_end_of_scan(None).await.unwrap();
         if *scanning.borrow() && channel_rx.borrow().is_some() {
             scanning.send(false).unwrap();
-            let channel = channel_rx.borrow().unwrap() as u32;
-            let frame = conn
-                .scan_frame_data_grab(channel, LineDir::Forward)
-                .await
-                .unwrap();
-            let size = [
-                frame.scan_data.size[0] as u32,
-                frame.scan_data.size[1] as u32,
-            ];
-            let frame_info = conn.scan_frame_get().await.unwrap();
-            let transform = transform_from_frame(&frame_info);
-            let props = conn.scan_props_get().await.unwrap();
-            let name = format!("{}({})", props.series_name, stamp_num);
-            stamp_num += 1;
-            let mut image = StaticImage::new(&encoder, size, transform, |buf| {
-                buf.copy_from_slice(&frame.scan_data.data)
-            });
-            image.channel = Some(channel as usize);
-            image.name = name;
-            image.signal_names = signal_names_rx.borrow().clone();
-            image.channels = vec![channel as usize];
-            frame_tx.send(image).unwrap();
-            ctx.request_repaint();
+            if let Some(frame) = frame_rx.borrow().as_ref() {
+                if !frame_early_exited(frame) {
+                    stamp_tx.send(Some(frame.clone())).unwrap();
+                    ctx.request_repaint();
+                }
+            }
         }
     }
+}
+
+fn frame_early_exited(frame: &FrameDataGrabResponse) -> bool {
+    let trailing_nans = match frame.scan_dir {
+        nanonis_tcp::ScanDir::Down => frame
+            .scan_data
+            .data
+            .iter()
+            .step_by(frame.scan_data.size[1])
+            .rev()
+            .take_while(|v| v.is_nan())
+            .count(),
+        nanonis_tcp::ScanDir::Up => frame
+            .scan_data
+            .data
+            .iter()
+            .step_by(frame.scan_data.size[1])
+            .take_while(|v| v.is_nan())
+            .count(),
+    };
+    trailing_nans > frame.scan_data.size[0] * 3 / 4
 }
 
 async fn line_worker(
     addr: impl ToSocketAddrs,
     ctx: egui::Context,
     scanning: watch::Sender<bool>,
-    frame_tx: std::sync::mpsc::Sender<FrameDataGrabResponse>,
+    frame_tx: watch::Sender<Option<FrameDataGrabResponse>>,
     channel_rx: watch::Receiver<Option<usize>>,
 ) {
     let mut conn = nonblocking::NanonisTcp::new(addr).await.unwrap();
@@ -215,13 +231,14 @@ async fn line_worker(
         let resp = conn.scan_wait_end_of_line(None).await.unwrap();
         match resp.movement_type {
             ScanMovementType::Scan(LineDir::Forward) => {
-                if *scanning.borrow() && channel_rx.borrow().is_some() {
+                if channel_rx.borrow().is_some() {
+                    scanning.send(true).unwrap();
                     let channel = channel_rx.borrow().unwrap() as u32;
                     let frame = conn
                         .scan_frame_data_grab(channel, LineDir::Forward)
                         .await
                         .unwrap();
-                    frame_tx.send(frame).unwrap();
+                    frame_tx.send(Some(frame)).unwrap();
                     ctx.request_repaint();
                 }
             }
